bd5029a000211838a46d86838d06d5e8688a6804
Aleksandra Makhrova <amakhro@gmail.com>
2022-07-06 13:19:28 -0700

    Initial Commit

---
+++ SolarSwiftUI/SolarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSwiftUIApp.swift
+//  SolarSwiftUI
+//
+//  Created by antikiller on 06.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SolarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
